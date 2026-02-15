f441d9601f0b997a3136787d367833c3ef8818b6
Abhishek Ritu <abhishekritu810219@gmail.com>
2026-02-15 21:56:54 +0530

    check1

---
+++ src/main/java/com/example/tiamedsadmin/TiamedsAdminApplication.java
@@ -0,0 +1,15 @@
+package com.example.tiamedsadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiamedsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiamedsAdminApplication.class, args);
+    }
+
+
+
+}
---
+++ src/test/java/com/example/tiamedsadmin/TiamedsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiamedsadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiamedsAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
