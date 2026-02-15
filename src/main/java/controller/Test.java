8112bf3ce5f63b484a52bed716a93cf10901ec0a
Abhishek Ritu <abhishekritu810219@gmail.com>
2026-02-15 22:00:39 +0530

    crete test branch

--- src/main/java/controller/Test.java
+++ src/main/java/controller/Test.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test {
 
-
     @GetMapping("/health")
     public String healthCheck() {
         return "Server is healthy!";
