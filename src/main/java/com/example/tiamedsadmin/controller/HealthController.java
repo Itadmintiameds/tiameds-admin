ee7c461550c2aa5e06ffa750ec12edb7fbf8cc94
Abhishek Ritu <abhishekritu810219@gmail.com>
2026-02-17 16:00:54 +0530

    fix yml file

--- src/main/java/com/example/tiamedsadmin/controller/HealthController.java
+++ src/main/java/com/example/tiamedsadmin/controller/HealthController.java
@@ -1,11 +1,10 @@
-package controller;
-
+package com.example.tiamedsadmin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Test {
+public class HealthController {
 
     @GetMapping("/health")
     public String healthCheck() {
